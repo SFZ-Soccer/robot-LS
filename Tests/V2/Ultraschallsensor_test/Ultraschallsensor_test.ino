cc107c5997324e20dffac983212f07447fef2e43
NIIK-10 <sonntag.nik@gmail.com>
2026-03-02 17:38:52 +0100

    Ultraschallsensor test hinzugefügt

---
+++ Tests/V2/Ultraschallsensor_test/Ultraschallsensor_test.ino
@@ -0,0 +1,61 @@
+#define TRIG_PIN_links A13
+#define ECHO_PIN_links A7
+#define TRIG_PIN_rechts A14
+#define ECHO_PIN_rechts A8
+
+int get_ultra_distance_links() { 
+  digitalWrite(TRIG_PIN_links, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_links, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_links, LOW);
+
+  // Echo-Zeit messen
+  long duration = pulseIn(ECHO_PIN_links, HIGH);
+
+  // Entfernung berechnen
+  float distance = (duration * 0.0343) / 2;
+  /*
+  Serial.print("Entfernung: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  */
+
+  return distance;
+}
+
+int get_ultra_distance_rechts() { 
+  digitalWrite(TRIG_PIN_rechts, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_rechts, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_rechts, LOW);
+
+  // Echo-Zeit messen
+  long duration = pulseIn(ECHO_PIN_rechts, HIGH);
+
+  // Entfernung berechnen
+  float distance = (duration * 0.0343) / 2;
+  /*
+  Serial.print("Entfernung: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  */
+
+  return distance;
+}
+
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(TRIG_PIN_rechts, OUTPUT);
+  pinMode(ECHO_PIN_rechts, INPUT);
+  pinMode(TRIG_PIN_links, OUTPUT);
+  pinMode(ECHO_PIN_links, INPUT);
+}
+
+void loop() {
+  // Trigger-Puls senden
+  Serial.println(get_ultra_distance_rechts());
+}
