c90c4ead7ec0aa8de826a8b572478cdbaf70702c
NIIK-10 <sonntag.nik@gmail.com>
2026-03-09 18:12:25 +0100

    quick Test 3 Funktionen

--- src/V2/quick_test/quick_test.ino
+++ src/V2/quick_test/quick_test.ino
@@ -1,8 +1,192 @@
+/*
+  1 = get ir Values
+  2 = ultraschall r+l
+  3 = motor
+*/
+
+
+
+
 #include <MeAuriga.h>
+
+
+
+MeEncoderOnBoard m1(SLOT1); // rechts
+MeEncoderOnBoard m2(SLOT2); // links
+
+void move(int m1speed, int m2speed) { //ohne Encoder //rechts, links
+  m1.setMotorPwm(m1speed * 1 * -1); //Seiten Stimmen nicht unbedingt
+  m2.setMotorPwm(m2speed * 1); //speedfaktor gelöscht
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#define TRIG_PIN_links A13
+#define ECHO_PIN_links A7
+#define TRIG_PIN_rechts A8
+#define ECHO_PIN_rechts A14
+
+int get_ultra_distance_links() { 
+  digitalWrite(TRIG_PIN_links, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_links, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_links, LOW);
+
+  // Echo-Zeit messen
+  long duration = pulseIn(ECHO_PIN_links, HIGH);
+
+  // Entfernung berechnen
+  float distance = (duration * 0.0343) / 2;
+  /*
+  Serial.print("Entfernung: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  */
+
+  return distance;
+}
+
+int get_ultra_distance_rechts() { 
+  digitalWrite(TRIG_PIN_rechts, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN_rechts, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN_rechts, LOW);
+
+  // Echo-Zeit messen
+  long duration = pulseIn(ECHO_PIN_rechts, HIGH);
+
+  // Entfernung berechnen
+  float distance = (duration * 0.0343) / 2;
+  /*
+  Serial.print("Entfernung: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+  */
+
+  return distance;
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------
+
+#define NUM_SENSORS 12
+
+float irValue[NUM_SENSORS] = {0};
+
+const float irYcon[NUM_SENSORS] = { 
+    1.0000, 0.8660, 0.5000, 0.0000, -0.5000, -0.8660, 
+   -1.0000, -0.8660, -0.5000, -0.0000,  0.5000,  0.8660 
+};
+
+const float irXcon[NUM_SENSORS] = { 
+    0.0000,  0.5000,  0.8660,  1.0000,  0.8660,  0.5000, 
+    0.0000, -0.5000, -0.8660, -1.0000, -0.8660, -0.5000 
+};
+
+long zeit, zeitA, zeitN;
+long zeitH, zeitL;
+long messungen, highs, lows, wechsel;
+bool zustand;
+
+float powerY = 0;
+float powerX = 0;
+
+void get_irValue() {
+
+  // Pins 2–9
+  for (int i = 2; i <= 9; i++) {
+
+    zeitA = micros();
+    zeitH = 0;
+    zeitL = 0;
+    wechsel = 0;
+
+    zustand = digitalRead(i);
+    unsigned long startZeit = micros();
+
+    while (wechsel < 10 && micros() - startZeit < 3000) {
+      if (zustand != digitalRead(i)) {
+        zeitN = micros();
+        if (zustand == HIGH) zeitL += zeitN - zeitA;
+        else zeitH += zeitN - zeitA;
+
+        zustand = !zustand;
+        wechsel++;
+        zeitA = zeitN;
+      }
+    }
+
+    int sensorIndex = i - 2;
+    irValue[sensorIndex] = zeitH / 13.0;
+  }
+
+  // Pins 30–33
+  for (int i = 30; i <= 33; i++) {
+
+    zeitA = micros();
+    zeitH = 0;
+    zeitL = 0;
+    wechsel = 0;
+
+    zustand = digitalRead(i);
+    unsigned long startZeit = micros();
+
+    while (wechsel < 10 && micros() - startZeit < 3000) {
+      if (zustand != digitalRead(i)) {
+        zeitN = micros();
+        if (zustand == HIGH) zeitL += zeitN - zeitA;
+        else zeitH += zeitN - zeitA;
+
+        zustand = !zustand;
+        wechsel++;
+        zeitA = zeitN;
+      }
+    }
+
+    int sensorIndex = (i - 30) + 8;
+    irValue[sensorIndex] = zeitH / 13.0;
+  }
+
+  // Debug-Ausgabe 
+  
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    Serial.print("Sensor ");
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(irValue[i]);
+  }
+  Serial.println("------------------------"); 
+}
+
+
+
+
+
+//__________________________________________________________________________________________________________________
+
+
+
+
 
 void setup()
 {
   Serial.begin(9600);
+
+  for (int i = 2; i <= 9; i++) {
+    pinMode(i, INPUT);
+  }
+  for (int i = 30; i <= 33; i++) {
+    pinMode(i, INPUT);
+  }
+
+  pinMode(TRIG_PIN_rechts, OUTPUT);
+  pinMode(ECHO_PIN_rechts, INPUT);
+  pinMode(TRIG_PIN_links, OUTPUT);
+  pinMode(ECHO_PIN_links, INPUT);
 }
 
 void loop()
@@ -12,34 +196,28 @@ void loop()
     char a = Serial.read();
     switch(a)
     {
-      case '0':
-      Encoder_1.setTarPWM(0);
-      Encoder_2.setTarPWM(0);
-      break;
       case '1':
-      Encoder_1.setTarPWM(100);
-      Encoder_2.setTarPWM(-100);
+      get_irValue();
       break;
       case '2':
-      Encoder_1.setTarPWM(200);
-      Encoder_2.setTarPWM(-200);
+      Serial.print(get_ultra_distance_links());
+      Serial.print("  ");
+      Serial.println(get_ultra_distance_rechts());
       break;
-      case '3':
-      Encoder_1.setTarPWM(255);
-      Encoder_2.setTarPWM(-255);
-      break;
-      case '4':
-      Encoder_1.setTarPWM(-100);
-      Encoder_2.setTarPWM(100);
-      break;
-      case '5':
-      Encoder_1.setTarPWM(-200);
-      Encoder_2.setTarPWM(200);
-      break;
-      case '6':
-      Encoder_1.setTarPWM(-255);
-      Encoder_2.setTarPWM(255);
-      break;
+      case'3':
+      move(255,255);
+      Serial.println("255,255");
+      delay(1900);
+      move(0,255);
+      Serial.println("0,255");
+      delay(1900);
+      move(255,0);
+      Serial.println("255,0");
+      delay(1900);
+      move(-100,-100);
+      Serial.println("-100,-100");
+      delay(1900);
+      move(0,0);
       default:
       break;
     }
