878974f7c0faf2d4bd2eb462279899c89359b210
NIIK-10 <sonntag.nik@gmail.com>
2026-03-09 17:55:11 +0100

    grundstruktur im quick_test programm

--- src/V2/quick_test/quick_test.ino
+++ src/V2/quick_test/quick_test.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <MeAuriga.h>
 
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if(Serial.available())
+  {
+    char a = Serial.read();
+    switch(a)
+    {
+      case '0':
+      Encoder_1.setTarPWM(0);
+      Encoder_2.setTarPWM(0);
+      break;
+      case '1':
+      Encoder_1.setTarPWM(100);
+      Encoder_2.setTarPWM(-100);
+      break;
+      case '2':
+      Encoder_1.setTarPWM(200);
+      Encoder_2.setTarPWM(-200);
+      break;
+      case '3':
+      Encoder_1.setTarPWM(255);
+      Encoder_2.setTarPWM(-255);
+      break;
+      case '4':
+      Encoder_1.setTarPWM(-100);
+      Encoder_2.setTarPWM(100);
+      break;
+      case '5':
+      Encoder_1.setTarPWM(-200);
+      Encoder_2.setTarPWM(200);
+      break;
+      case '6':
+      Encoder_1.setTarPWM(-255);
+      Encoder_2.setTarPWM(255);
+      break;
+      default:
+      break;
+    }
+  }
 }
+
